96133a30101afcb71b1c10f07db8bcd015a291bb
Anuj khare <anujkhare92@gmail.com>
2025-11-28 22:54:23 +0530

    Configured Test to pick environment properties instead of hardcoding

--- src/test/java/com/example1/demo/DemoApplicationTests.java
+++ src/test/java/com/example1/demo/DemoApplicationTests.java
@@ -2,14 +2,25 @@ package com.example1.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 
 @SpringBootTest
-@TestPropertySource(properties = "notification.type=sms")
 class DemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        String notificationType = System.getenv("Notification_Type");
+        if (notificationType != null && !notificationType.isEmpty()) {
+            registry.add("notification.type", () -> notificationType);
+        } else {
+            //setting default notification type as sms
+            registry.add("notification.type", () -> "sms");
+        }
+    }
+
+    @Test
+    void contextLoads() {
+    }
 
 }
